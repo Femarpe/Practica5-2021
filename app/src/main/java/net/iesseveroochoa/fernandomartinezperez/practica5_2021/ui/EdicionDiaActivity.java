84b1aac2ecd06fc74d94d77579413d9f6df2ab34
ferpuntoexe <kururu.fm@gmail.com>
2022-02-21 14:41:25 +0100

    21022022

--- app/src/main/java/net/iesseveroochoa/fernandomartinezperez/practica5_2021/ui/EdicionDiaActivity.java
+++ app/src/main/java/net/iesseveroochoa/fernandomartinezperez/practica5_2021/ui/EdicionDiaActivity.java
@@ -1,21 +1,33 @@
 package net.iesseveroochoa.fernandomartinezperez.practica5_2021.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import net.iesseveroochoa.fernandomartinezperez.practica5_2021.R;
 import net.iesseveroochoa.fernandomartinezperez.practica5_2021.model.DiaDiario;
@@ -23,20 +35,27 @@ import net.iesseveroochoa.fernandomartinezperez.practica5_2021.model.DiaDiario;
 import java.util.Calendar;
 import java.util.Date;
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission_group.CAMERA;
+
 public class EdicionDiaActivity extends AppCompatActivity {
 
     public final static String EXTRA_DIA = "EXTRA_DIA";
-
+    private static final int STATUS_CODE_SELECCION_IMAGEN = 300;
+    private static final int MY_PERMISSIONS = 100;
     private EditText etResumen;
     private ImageButton ibfecha;
     private EditText etmContenido;
     private FloatingActionButton fabGuardar;
     private Spinner spValorDia;
     private TextView tvFecha;
-
+    private Uri uriFoto = null;
+    private ImageView ivFotoDia;
     boolean esEdicion;
+    private Button addImg;
     DiaDiario diaDiarioAnt;
     DiaDiario dia;
+    private View clPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +70,8 @@ public class EdicionDiaActivity extends AppCompatActivity {
         spValorDia = findViewById(R.id.spValorDia);
         tvFecha = findViewById(R.id.tvFecha);
         diaDiarioAnt = getIntent().getParcelableExtra(EXTRA_DIA);
+        ivFotoDia = findViewById(R.id.ivFotoDia);
+        addImg = findViewById(R.id.btAddImg);
         final Date[] fechaselec = {new Date()};
 
         if (diaDiarioAnt != null) {
@@ -72,10 +93,9 @@ public class EdicionDiaActivity extends AppCompatActivity {
                                 monthOfYear, int dayOfMonth) {
                             Calendar calendar = Calendar.getInstance();
                             calendar.set(year, monthOfYear, dayOfMonth);
-                            tvFecha.setText(dayOfMonth+"/"+ monthOfYear+"/"+year);
-                            //diaDiarioAnt.setFecha(calendar.getTime());
+                            tvFecha.setText(dayOfMonth + "/" + monthOfYear + "/" + year);
                             fechaselec[0] = calendar.getTime();
-                            if (esEdicion == true){
+                            if (esEdicion == true) {
                                 diaDiarioAnt.setFecha(fechaselec[0]);
                             }
                         }
@@ -112,13 +132,20 @@ public class EdicionDiaActivity extends AppCompatActivity {
                 int valordia = Integer.parseInt(spValorDia.getSelectedItem().toString());
 
                 if (esEdicion == true) {
-                    diaDiario = new DiaDiario(diaDiarioAnt.getId(), diaDiarioAnt.getFecha(), valordia, etResumen.getText().toString(), etmContenido.getText().toString());
+                    diaDiario = new DiaDiario(diaDiarioAnt.getId(),
+                            diaDiarioAnt.getFecha(),
+                            valordia,
+                            etResumen.getText().toString(),
+                            etmContenido.getText().toString());
 
                 } else {
-                    diaDiario = new DiaDiario(fechaselec[0], valordia, etResumen.getText().toString(), etmContenido.getText().toString());
+                    diaDiario = new DiaDiario(fechaselec[0],
+                            valordia,
+                            etResumen.getText().toString(),
+                            etmContenido.getText().toString());
 
                 }
-
+                ocultarTeclado();
                 intent.putExtra(EXTRA_DIA, diaDiario);
                 setResult(Activity.RESULT_OK, intent);
                 finish();
@@ -126,6 +153,11 @@ public class EdicionDiaActivity extends AppCompatActivity {
         });
 
 
+        addImg.setOnClickListener(view -> {
+            if (pedirPermosos()) {
+                muestraOpcionesImagen();
+            }
+        });
     }
 
     private int getIndex(Spinner spinner, String valor) {
@@ -135,5 +167,88 @@ public class EdicionDiaActivity extends AppCompatActivity {
             }
         }
         return 0;
+    }
+
+    private void elegirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent.createChooser(intent, "Seleccione una imagen"), STATUS_CODE_SELECCION_IMAGEN);
+    }
+
+    private void muestraFoto() {
+        Glide.with(this)
+                .load(uriFoto)
+                .into(ivFotoDia);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case STATUS_CODE_SELECCION_IMAGEN:
+                    uriFoto = data.getData();
+                    muestraFoto();
+                    break;
+            }
+        }
+    }
+
+    private void muestraOpcionesImagen() {
+        final CharSequence[] option = {"Hacer foto", "Elegir de la galería",
+                getString(android.R.string.cancel)};
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(android.R.string.dialog_alert_title);
+        builder.setItems(option, (dialog, which) -> {
+            switch (which) {
+                case 0:
+                    abrirCamara();
+                    break;
+                case 1:
+                    elegirGaleria();
+                    break;
+            }
+            dialog.dismiss();
+        });
+        builder.show();
+    }
+
+    private boolean pedirPermosos() {
+
+        if ((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED))
+            return true;
+
+        else if ((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))
+                || (shouldShowRequestPermissionRationale(CAMERA))) {
+
+            Snackbar.make(clPrincipal,
+                    "Es necesario acptar los permisos para esta funcion",
+                    Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok,
+                    v -> requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE,
+                            CAMERA}, MY_PERMISSIONS)).show();
+        } else {
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA},
+                    MY_PERMISSIONS);
+        }
+        return false;
+    }
+
+
+    private void ocultarTeclado() {
+        InputMethodManager inputMethodManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(etResumen.getWindowToken(), 0);
+            inputMethodManager.hideSoftInputFromWindow(etmContenido.getWindowToken(), 0);
+        }
+    }
+
+    public void abrirCamara(){
+
     }
 }
